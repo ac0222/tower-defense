4773262ce443a059859022faf17b919998606e3e
acheung <adrian.che0222@gmail.com>
2021-09-15 20:27:19 +1000

    add restart button ui

--- Assets/Scripts/UI/WaveControlPanel.cs
+++ Assets/Scripts/UI/WaveControlPanel.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class WaveControlPanel : MonoBehaviour
 {
     public Button sendNextWaveButton;
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
     {
         sendNextWaveButton.onClick.AddListener(() => SendWave());
+        restartButton.onClick.AddListener(() => RestartListener());
     }
 
     void SendWave()
     {
         GameController.Instance.CreateNextWave();    
+    }
+
+    void RestartListener()
+    {
+        Debug.Log("restart clicked");
     }
 }
