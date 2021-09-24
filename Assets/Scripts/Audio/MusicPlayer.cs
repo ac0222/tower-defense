f4c3feb3f3496abb27d44ad1b649ed6c5bab6672
acheung <adrian.che0222@gmail.com>
2021-09-24 10:45:26 +1000

    dont reset music on scene reload

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MusicPlayer : MonoBehaviour 
+{
+    private static MusicPlayer instance = null;
+ 
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+ }
